d62f6af0c8eaf5209fe33768f91d91ca5044bdda
Dejan Dokic <doxa997@gmail.com>
2019-04-10 14:13:09 +0200

    adding hotel admin

---
+++ tim14/src/main/java/com/isamrs/tim14/repository/IHotelAdminRepository.java
@@ -0,0 +1,9 @@
+package com.isamrs.tim14.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isamrs.tim14.model.HotelAdmin;
+
+public interface IHotelAdminRepository extends JpaRepository<HotelAdmin, Integer>{
+
+}
