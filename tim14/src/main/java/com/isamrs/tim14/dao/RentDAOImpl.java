280326bb3c0f0322ed290bfadc7d5cb6ce03934c
= <katarina.aleksic97@gmail.com>
2019-04-24 20:55:44 +0200

    Make connection between rent-a-car entities

---
+++ tim14/src/main/java/com/isamrs/tim14/dao/RentDAOImpl.java
@@ -0,0 +1,79 @@
+package com.isamrs.tim14.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.isamrs.tim14.model.RentACar;
+
+@Repository
+public class RentDAOImpl implements RentDAO{
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public RentDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	
+	@Override
+	@Transactional
+	public List<RentACar> getRents() {
+		
+		Query query = entityManager.createQuery("SELECT rent FROM RentACar rent");
+		List<RentACar> result = query.getResultList();
+		return result;
+	}
+
+	@Override
+	@Transactional
+	public RentACar save(RentACar rent) {
+		Query query = entityManager.createQuery("SELECT rent FROM RentACar rent WHERE lower(rent.name) LIKE :rentName");
+		query.setParameter("rentName", rent.getName());
+		List<RentACar> result = query.getResultList();
+		
+		if(result.size() == 0) {
+			entityManager.persist(rent);
+			return rent;
+		}else {
+			return null;
+		}
+	}
+
+	@Override
+	@Transactional
+	public RentACar getRent(int id) {
+		Query query = entityManager.createQuery("SELECT rent FROM RentACar rent WHERE rent.id = :rentId");
+		query.setParameter("rentId",id);
+		List<RentACar> result = query.getResultList();
+		
+		if(result.size() == 0) {
+			return null;
+		}
+		
+		return result.get(0);
+	}
+
+	@Override
+	@Transactional
+	public void deleteRent(int id) {
+		
+	}
+
+
+	@Override	
+	@Transactional
+	public List<RentACar> getRentSearch(String search) {
+		Query query = entityManager.createQuery("SELECT r FROM RentACar r WHERE r.name LIKE :rentName");
+		query.setParameter("rentName", "%"+search+"%");
+		List<RentACar> result = query.getResultList();
+		return result;
+	}
+
+}
---
+++ tim14/src/main/java/com/isamrs/tim14/rest/RentACarRest.java
@@ -0,0 +1,80 @@
+package com.isamrs.tim14.rest;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isamrs.tim14.dao.RentDAO;
+import com.isamrs.tim14.model.Hotel;
+import com.isamrs.tim14.model.RentACar;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class RentACarRest {
+	
+	private RentDAO rentDAO;
+	
+	@Autowired
+	public RentACarRest(RentDAO rentDAO) {
+		this.rentDAO = rentDAO;
+	}
+	
+	@RequestMapping(
+			value = "/rentacars",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<RentACar>> getRents(){
+		
+		Collection<RentACar> rents = rentDAO.getRents();
+		
+		return new ResponseEntity<Collection<RentACar>>(rents, HttpStatus.OK);
+	}
+	
+	@RequestMapping(
+			value = "/rentacars/{rentID}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<RentACar> getRentACar(@PathVariable Integer rentID) {
+		RentACar rent =  rentDAO.getRent(rentID);
+		if(rent == null) {
+			return new ResponseEntity<RentACar>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<RentACar>(rent, HttpStatus.OK);
+	}
+	
+	@RequestMapping(
+			value = "/rentacars",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<RentACar> saveRent(@RequestBody RentACar rentacar) {
+		RentACar newRent = rentDAO.save(rentacar);
+		if(newRent == null) {
+			return new ResponseEntity<RentACar>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<RentACar>(newRent, HttpStatus.CREATED);
+	}
+	
+	
+	@RequestMapping(
+			value = "/rentsSearch/{rentName}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<RentACar>> getRentSearch(@PathVariable String rentName){
+		
+		Collection<RentACar> rents = rentDAO.getRentSearch(rentName);
+		
+		return new ResponseEntity<Collection<RentACar>>(rents, HttpStatus.OK);
+	}
+	
+}
