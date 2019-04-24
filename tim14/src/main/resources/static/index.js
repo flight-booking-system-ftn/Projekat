1f83010957617d2cf9dbd839db9a5fe58d3cebea
Dejan Dokic <doxa997@gmail.com>
2019-04-24 22:21:09 +0200

    display hotels

--- tim14/src/main/resources/static/index.js
+++ tim14/src/main/resources/static/index.js
@@ -1,40 +1,63 @@
-$("#new_airline").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "airline.html"
-})
+$(document).ready(function(){
+    
+    renderAirlineTable();
+    renderHotelTable();
 
-$("#new_airline").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "airline.html"
-})
+    $(document).on('click','#addAirlineBtn',function(){
+        $(location).attr('href',"/airline.html");
+    });
+    $(document).on('click','#addHotelBtn',function(){
+        $(location).attr('href',"/hotel.html");
+    });
+    $(document).on('click','#addRentACarBtn',function(){
+        $(location).attr('href',"/rentacar.html");
+    });
+   
+    $(document).on('click','#addFlightBtn',function(){
+        $(location).attr('href',"/newFlight.html");
+    });
+    $(document).on('click','#loginUserBtn',function(){
+        $(location).attr('href',"/login.html");
+    });
+    
+    
+    
 
-$("#new_hotel").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "hotel.html"
-})
+    $(document).on('click','#addAirlineAdminBtn',function(){
+        $(location).attr('href',"/newAirlineAdmin.html");
+    });
+    $(document).on('click','#addHotelAdminBtn',function(){
+        $(location).attr('href',"/newHotelAdmin.html");
+    });
+    $(document).on('click','#addRentACarAdminBtn',function(){
+        $(location).attr('href',"/newRentACarAdmin.html");
+    });
 
-$("#new_rent_a_car").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "rentacar.html"
-})
 
-$("#edit_user").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "airline.html"
-})
+    
 
-$("#edit_airline").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "airline.html"
-})
+});
 
-$("#edit_rentacar").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "airline.html"
-})
+var renderAirlineTable = function(){
+    $('#airlineTable').html(`<tr><th>Name</th><th>Description</th></tr>`);
+    $.get("/api/airlines", function(data){
+        console.log("ssAirlines: ", data);
+        for(var i=0;i<data.length;i++){
+            var red = data[i];
+            $('#airlineTable tr:last').after(`<tr><td>${red.name}</td><td>${red.description}</td></tr>`);
+        }
+    });
+}
 
-$("#new_vehicle").submit(function(e) {
-    e.preventDefault();
-    window.location.href = "vehicle.html"
-})
 
+var renderHotelTable = function(){
+    $('#hotelTable').html(`<tr><th>Name</th><th>Description</th><th></th></tr>`);
+    $.get("/api/hotels", function(data){
+        console.log("Hotels: ", data);
+        for(var i=0;i<data.length;i++){
+            var red = data[i];
+            var btnID = "hotelDetailViewBtn" + red.id;
+            $('#hotelTable tr:last').after(`<tr><td>${red.name}</td><td>${red.description}</td><td><button id=${btnID}>More details</button></td></tr>`);
+        }
+    });
+}
